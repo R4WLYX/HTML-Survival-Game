b3bf6463d73e39ec93da7f4c9ba1fb91a48bfbb2
R4WLYX <116930031+R4WLYX@users.noreply.github.com>
2022-11-20 10:12:35 +0700

    Update slimes.js
    Changed slimes to class instead of object
    Better slime behavior and animations

--- slimes.js
+++ slimes.js
@@ -1,40 +1,45 @@
-var slimes = {
+class slimes {
     // Margin
-    marginX: 35,
-    marginY: 25,
+    marginX = 35;
+    marginY = 25;
 
     // Position
-    x: 0, 
-    y: 0,
+    x = 0; 
+    y = 0;
 
-    originX: 0,
-    originY: 0,
+    originX = 0;
+    originY = 0;
 
-    trueX: 0,
-    trueY: 0,
+    trueX = 0;
+    trueY = 0;
 
     // Size
-    width: 120,
-    height: 120,
+    width = 120;
+    height = 120;
 
-    trueWidth: 45,
-    trueHeight: 50,
+    trueWidth = 45;
+    trueHeight = 50;
 
     // Stats
-    health: 60,
-    mass: 30, // kg
-    speed: { // m/s
-        max: 1.25, 
+    health = 60;
+    mass = 30; // kg
+    speed = { // m/s
+        max: 1.25,
         current: 0
-    },
+    };
 
     // Animations
-    animation: new Image(),
-    interval: 6,
-    yOffset: 2,
+    animation = new Image();
+    animationFrame = 0;
+    frameOffset = 0;
+    interval = 6;
+    yOffset = 2;
 
     // Functions
-    Init: function({health, mass, maxSpeed, interval, yOffset}) {
+    constructor(data) {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+
         this.originX = this.x + this.width/2;
         this.originY = this.y + this.height;
 
@@ -43,13 +48,14 @@ var slimes = {
 
         this.animation.src = "animations/slimes.png";
 
-        this.health = health;
-        this.mass = mass;
-        this.speed.max = maxSpeed;
-        this.interval = interval;
-        this.yOffset = yOffset;
-    },
-    Update: function(frame) {
+        this.health = data.health;
+        this.mass = data.mass;
+        this.speed.max = data.maxSpeed;
+        this.frameOffset = Math.floor(Math.random()*7);
+        this.interval = data.interval;
+        this.yOffset = data.yOffset;
+    };
+    Update = function(frame) {
         // Friction
         this.ApplyForce(-2.5);
         
@@ -61,41 +67,37 @@ var slimes = {
 
         this.trueX = this.x + this.marginX;
         this.trueY = this.y + this.marginY;
-    },
-    Move: function(frame) {
-        let speed;
-        let animationFrame;
-
+    };
+    Move = function(frame) {
         this.ApplyForce(7); 
-        speed = this.speed.current;
-        animationFrame = Math.floor(frame/this.interval);
+        this.animationFrame = Math.floor(frame/this.interval) + this.frameOffset;
 
-        if (animationFrame > 7) {
-            animationFrame -= (8 * Math.floor(animationFrame/8));
+        if (this.animationFrame > 7) {
+            this.animationFrame -= (8 * Math.floor(this.animationFrame/8));
         }
         
-        if (animationFrame > 2) {
+        if (this.animationFrame > 3) {
             // Move Up
             if (this.originY > player.originY) {
-                this.y -= speed;
+                this.y -= this.speed.current;
             }
             // Move Down
             if (player.originY > this.originY) {
-                this.y += speed;
+                this.y += this.speed.current;
             }
             // Move Right
             if (player.originX > this.originX) {
-                this.x += speed;
+                this.x += this.speed.current;
             }
             // Move Left
             if (this.originX > player.originX) {
-                this.x -= speed;
+                this.x -= this.speed.current;
             }
         }
 
-        this.Animate(frame);
-    },
-    ApplyForce: function(force) {
+        this.Animate();
+    };
+    ApplyForce = function(force) {
         this.speed.current += force/this.mass;
         if (this.speed.current >= this.speed.max) {
             this.speed.current = this.speed.max;
@@ -103,135 +105,8 @@ var slimes = {
         if (this.speed.current < 0) {
             this.speed.current = 0;
         }
-    },
-    Animate: function(frame) {
-        let animationFrame = 0;
-
-        animationFrame = Math.floor(frame/this.interval);
-
-        if (animationFrame > 7) {
-            animationFrame -= (8 * Math.floor(animationFrame/8));
-        }
-
-        ctx.drawImage(this.animation, animationFrame*this.width, this.yOffset*this.height, this.width, this.width, this.x, this.y, this.width, this.height);
+    };
+    Animate = function() {
+        ctx.drawImage(this.animation, this.animationFrame*this.width, this.yOffset*this.height, this.width, this.width, this.x, this.y, this.width, this.height);
     }
 }
-
-// class slimes {
-//     // Margin
-//     marginX = 35;
-//     marginY = 25;
-
-//     // Position
-//     x = 0; 
-//     y = 0;
-
-//     originX = 0;
-//     originY = 0;
-
-//     trueX = 0;
-//     trueY = 0;
-
-//     // Size
-//     width = 120;
-//     height = 120;
-
-//     trueWidth = 45;
-//     trueHeight = 50;
-
-//     // Stats
-//     health = 60;
-//     mass = 30; // kg
-//     speed = { // m/s
-//         max: 1.25, 
-//         current: 0
-//     };
-
-//     // Animations
-//     animation = new Image();
-//     interval = 6;
-//     yOffset = 2;
-
-//     // Functions
-//     Init = function({health, mass, maxSpeed, interval, yOffset}) {
-//         this.originX = this.x + this.width/2;
-//         this.originY = this.y + this.height;
-
-//         this.trueX = this.x + this.marginX;
-//         this.trueY = this.y + this.marginY;
-
-//         this.animation.src = "animations/slimes.png";
-
-//         this.health = health;
-//         this.mass = mass;
-//         this.speed.max = maxSpeed;
-//         this.interval = interval;
-//         this.yOffset = yOffset;
-//     };
-//     Update = function(frame) {
-//         // Friction
-//         this.ApplyForce(-2.5);
-        
-//         // Move mob
-//         this.Move(frame);
-
-//         this.originX = this.x + this.width/2;
-//         this.originY = this.y + this.height/2;
-
-//         this.trueX = this.x + this.marginX;
-//         this.trueY = this.y + this.marginY;
-//     };
-//     Move = function(frame) {
-//         let speed;
-//         let animationFrame;
-
-//         this.ApplyForce(7); 
-//         speed = this.speed.current;
-//         animationFrame = Math.floor(frame/this.interval);
-
-//         if (animationFrame > 7) {
-//             animationFrame -= (8 * Math.floor(animationFrame/8));
-//         }
-        
-//         if (animationFrame > 2) {
-//             // Move Up
-//             if (this.originY > player.originY) {
-//                 this.y -= speed;
-//             }
-//             // Move Down
-//             if (player.originY > this.originY) {
-//                 this.y += speed;
-//             }
-//             // Move Right
-//             if (player.originX > this.originX) {
-//                 this.x += speed;
-//             }
-//             // Move Left
-//             if (this.originX > player.originX) {
-//                 this.x -= speed;
-//             }
-//         }
-
-//         this.Animate(frame);
-//     };
-//     ApplyForce = function(force) {
-//         this.speed.current += force/this.mass;
-//         if (this.speed.current >= this.speed.max) {
-//             this.speed.current = this.speed.max;
-//         }
-//         if (this.speed.current < 0) {
-//             this.speed.current = 0;
-//         }
-//     };
-//     Animate = function(frame) {
-//         let animationFrame = 0;
-
-//         animationFrame = Math.floor(frame/this.interval);
-
-//         if (animationFrame > 7) {
-//             animationFrame -= (8 * Math.floor(animationFrame/8));
-//         }
-
-//         ctx.drawImage(this.animation, animationFrame*this.width, this.yOffset*this.height, this.width, this.width, this.x, this.y, this.width, this.height);
-//     }
-// }
