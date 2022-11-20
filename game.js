ca488df99626f0ce850eb5c09cb3cf2fa937038c
R4WLYX <116930031+R4WLYX@users.noreply.github.com>
2022-11-20 10:11:13 +0700

    Update game.js
    Added support for multiple slimes

--- game.js
+++ game.js
@@ -15,43 +15,59 @@ addEventListener("keydown", (e) => {
 });
 addEventListener("keyup", (e) => {
     keyPressed[e.keyCode] = false;
-}); 
+});
 
-// let basic = slimes;
-// basic.Init({
-//     health: 60,
-//     mass: 30,
-//     maxSpeed: 1.25,
-//     interval: 6,
-//     yOffset: 0
-// });
+let basics = [];
+let basicData = {
+    health: 60,
+    mass: 30,
+    maxSpeed: 1.25,
+    interval: 6,
+    yOffset: 0
+};
+for (let i = 0; i < 8; i++) {
+    basics[i] = new slimes(basicData);
+}
 
-// let speedy = slimes;
-// speedy.Init({
-//     health: 40,
-//     mass: 27,
-//     maxSpeed: 1.5,
-//     interval: 5,
-//     yOffset: 1
-// });
+let speedies = [];
+let speedyData = {
+    health: 40,
+    mass: 27,
+    maxSpeed: 1.5,
+    interval: 5,
+    yOffset: 1
+};
+for (let i = 0; i < 8; i++) {
+    speedies[i] = new slimes(speedyData);
+}
 
-// let grumpy = slimes;
-// grumpy.Init({
-//     health: 75,
-//     mass: 38,
-//     maxSpeed: 1,
-//     interval: 6,
-//     yOffset: 2
-// });
+let grumpies = [];
+let grumpyData = {
+    health: 75,
+    mass: 38,
+    maxSpeed: 1,
+    interval: 8,
+    yOffset: 2
+};
+for (let i = 0; i < 8; i++) {
+    grumpies[i] = new slimes(grumpyData);
+}
 
 function update() {
     requestAnimationFrame(update);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     player.Update(keyPressed, frame);
-    basic.Update(frame);
-    speedy.Update(frame);
-    grumpy.Update(frame);
+
+    for (let i = 0; i < basics.length; i++) {
+        basics[i].Update(frame);
+    }
+    for (let i = 0; i < speedies.length; i++) {
+        speedies[i].Update(frame);
+    }
+    for (let i = 0; i < grumpies.length; i++) {
+        grumpies[i].Update(frame);
+    }
 
     frame++;
 }
